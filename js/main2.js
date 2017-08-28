194ae545cb833db98e8bdbea09807df3d8c8dbba
daiki <dik.piyo.piyo@gmail.com>
2017-08-28 23:04:46 +0900

    use css3drenderer

---
+++ js/main2.js
@@ -0,0 +1,25 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 5000);
+camera.position.z = 1000;
+
+const object = new THREE.CSS3DObject(document.getElementById('cube'));
+scene.add(object);
+
+const renderer = new THREE.CSS3DRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.domElement.style.position = 'absolute';
+document.getElementById('container').appendChild(renderer.domElement);
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
+
+animate();
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  render();
+}
+function render() {
+  renderer.render(scene, camera);
+}
